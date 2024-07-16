a560b739ca3e67a742641c210cd4b0fca53bb218
ericssbn <ericsieben@outlook.com>
2024-07-16 20:00:25 -0300

    adição de reduce e filter

--- scripts.js
+++ scripts.js
@@ -1,28 +1,52 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
-let myLi = ''
+const sumAll = document.querySelector('.sum-all')
+const filterVegan = document.querySelector('.filter-vegan')
 
+function showAll(productsArray) {
+    let myLi = ''
 
-function showAll() {
-
-    menuOptions.forEach((product) => {
+    productsArray.forEach((product) => {
         myLi = myLi + `
     <li>
     <img src=${product.src}>
     <p>${product.name}</p>
     <p class="item-price">R$:${product.price}</p>
-        </li
+        </li>
     `
     })
     list.innerHTML = myLi
 }
 
-function mapAll(){
-console.log('yesss')
+function mapAll() {
+    const newPrice = menuOptions.map((product) => ({
+        name: product.name,
+        price: product.price * 0.9,
+        vegan: product.vegan,
+        src: product.src,
 
+    }))
+    showAll(newPrice)
 }
 
+function sumAllItens() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
 
-buttonShowAll.addEventListener('click', showAll)
+    list.innerHTML = `
+    <li>
+    <p>O valor total dos itens é R$: ${totalValue}</p>
+        </li>
+    `}
+
+function filterVeganItens(){
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+showAll(filterJustVegan)
+}
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
 buttonMapAll.addEventListener('click', mapAll)
+sumAll.addEventListener('click', sumAllItens)
+filterVegan.addEventListener('clilck', filterVeganItens)
+
